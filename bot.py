7d456bb9b08a851aed6d7102b80ee78ec4447d69
EarlG <earl-gray@mail.ru>
2021-02-10 21:22:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,19 @@
-#from pyowm import OWM
-#from pyowm.utils import config
-#from pyowm.utils import timestamps
+import pyowm
 import telebot
 
+owm = pyowm.OWM('f70b2868746d0a1f0c27740e7031549a')
 bot = telebot.TeleBot("1587685684:AAE4Z9fZMpG337j1Z_pgbE1q5YmiCiKjwwg")
-#owm = OWM('f70b2868746d0a1f0c27740e7031549a')
-#mgr = owm.weather_manager()
 
 @bot.message_handler(content_types = ['text'])
 def send_echo( message ):
     place = message.text
     try:
-        #observation = mgr.weather_at_place( place )
-        #w = observation.weather
-        #temp = w.temperature('celsius')["temp"]
-        #detstat =  w.detailed_status
-        temp = 1
-        detstat = "1"
+        observation = owm.weather_at_place( place )
+        w = observation.get_weather()
+        temp = w.get_temperature('celsius')["temp"]
+        detstat =  w.get_detailed_status()
+        #temp = 1
+        #detstat = "1"
         answer = "В городе " + place + " температура "
         answer += str(temp)
         answer += "C, " + detstat
