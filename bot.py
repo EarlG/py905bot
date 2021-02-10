8c2ad70b8342c82379b46cf9d2cc69239cc24cf6
EarlG <earl-gray@mail.ru>
2021-02-10 21:05:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,25 @@
-from pyowm import OWM
-from pyowm.utils import config
-from pyowm.utils import timestamps
+#from pyowm import OWM
+#from pyowm.utils import config
+#from pyowm.utils import timestamps
 import telebot
 
 bot = telebot.TeleBot("1587685684:AAE4Z9fZMpG337j1Z_pgbE1q5YmiCiKjwwg")
-owm = OWM('f70b2868746d0a1f0c27740e7031549a')
-mgr = owm.weather_manager()
+#owm = OWM('f70b2868746d0a1f0c27740e7031549a')
+#mgr = owm.weather_manager()
 
 @bot.message_handler(content_types = ['text'])
 def send_echo( message ):
     place = message.text
     try:
-        observation = mgr.weather_at_place( place )
-        w = observation.weather
-        temp = w.temperature('celsius')["temp"]
+        #observation = mgr.weather_at_place( place )
+        #w = observation.weather
+        #temp = w.temperature('celsius')["temp"]
+        #detstat =  w.detailed_status
+        temp = 1
+        detstat = "1"
         answer = "В городе " + place + " температура "
         answer += str(temp)
-        answer += "C, " + w.detailed_status
+        answer += "C, " + detstat
     except Exception:
         answer = 'Не удалось определить место, '
         answer += 'напишите название города по английски или русски'
